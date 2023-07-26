a1a104a62620fc76b78daca3bc8a7806e7859307
yyyxon <130555434+yyyxon@users.noreply.github.com>
2023-07-26 17:35:56 +0900

    File input,output

--- LogIO.java
+++ LogIO.java
@@ -1,12 +1,17 @@
 package report;
 
 import java.awt.FileDialog;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -14,6 +19,7 @@ import javax.swing.JOptionPane;
 public class LogIO {
 	
 	private String filePath;
+	private List<String> logList;
 
 	//log파일인지 확인 후 파일 경로 저장
 	public void openLog() throws FileNotFoundException {
@@ -34,10 +40,22 @@ public class LogIO {
 			}			
 		}
 	 }
+	
+	//List에 LogData 저장
+	public List<String> readLog() throws IOException {
+		logList = new ArrayList<String>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(filePath))));
+		String logData = "";
+		while((logData = br.readLine()) != null) {
+			logList.add(logData);
+		}
+
+		return logList;
+	}
 	 
 	//레포트 생성
 	public void writeReport() throws IOException {
-		StringBuilder reportName = new StringBuilder("/report_").append(new Date().getTime()).append(".dat");
+		StringBuilder reportName = new StringBuilder("/report_").append(System.currentTimeMillis()).append(".dat");
 		File file = new File("e:/dev/report");
 		
 		//디렉토리 경로가 존재하지 않으면 디렉토리 생성
@@ -57,9 +75,14 @@ public class LogIO {
 		}
 	}
 	
+	//test한거 메인은 날리기~
 	public static void main(String[] args) {
 		try {
-			new LogIO().writeReport();
+			LogIO logio = new LogIO();
+			logio.openLog();
+			logio.writeReport();
+			//List<String> li = logio.readLog();
+			//System.out.println(li.get(969));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
