738a72503154e58123705ac2d9a00b8457091798
thonzyy <136280932+thonzyy@users.noreply.github.com>
2023-07-26 12:04:46 +0900

    Update readLogFile.java

--- readLogFile.java
+++ readLogFile.java
@@ -8,52 +8,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class readLogFile {
-	
-	public static List<Integer> readFile(File file) throws IOException {
-        List<String> logList = new ArrayList<>();
-        List<Integer> logList2 = new ArrayList<>();
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
+    public static List<Integer> readFile(File file) throws IOException {
+    	   List<Integer> logList2 = new ArrayList<>();
+
+    	    BufferedReader br = new BufferedReader(new FileReader(file));
+    	    int lineCounter = 0;
+
+    	    try {
+    	        String temp;
+
+    	        while ((temp = br.readLine()) != null) {
+    	            lineCounter++;
+
+    	            // [] 안에 있는 숫자 추출하여 logList2에 추가
+    	            int startIndex = temp.indexOf("[") + 1;
+    	            int endIndex = temp.indexOf("]");
+    	            if (startIndex > 0 && endIndex > startIndex) {
+    	                String numberStr = temp.substring(startIndex, endIndex);
+    	                try {
+    	                    int number = Integer.parseInt(numberStr);
+
+    	                    // 1000번째에서 1500번째 라인까지만 숫자를 logList2에 추가
+    	                    if (lineCounter >= 1000 && lineCounter <= 1500) {
+    	                        logList2.add(number);
+    	                    }
+    	                } catch (NumberFormatException e) {
+    	                    // 숫자로 변환할 수 없는 경우 무시
+    	                }
+    	            }
+    	        }
+    	    } finally {
+    	        if (br != null) {
+    	            br.close();
+    	        }
+    	    }
+
+    	    // logList2 반환
+    	    return logList2;
+    	}
+
+    public static void main(String[] args) {
+        File file = new File("e:/dev/sist_input_1.log");
+
         try {
-            String temp;
-
-            while ((temp = br.readLine()) != null) {
-                logList.add(temp + "\n");
-
-                // [] 안에 있는 숫자 추출하여 logList2에 추가
-                int startIndex = temp.indexOf("[") + 1;
-                int endIndex = temp.indexOf("]");
-                if (startIndex > 0 && endIndex > startIndex) {
-                    String numberStr = temp.substring(startIndex, endIndex);
-                    try {
-                        int number = Integer.parseInt(numberStr);
-                        logList2.add(number);
-                    } catch (NumberFormatException e) {
-                       
-                    }
-                }
+            List<Integer> extractedNumbers = readFile(file);
+            // logList2 출력
+            for (int num : extractedNumbers) {
+                System.out.println(num);
             }
-        } finally {
-            if (br != null) {
-                br.close();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        // logList2 출력
-        for (int num : logList2) {
-            System.out.println(num);
-        }
-
-        return logList2;
     }
-
-
-	public static void main(String[] args) {
-		File file = new File("e:/dev/sist_input_1.log"); 
-		readLogFile rlf =new readLogFile();
-		
-		
-
-	}
-
 }
