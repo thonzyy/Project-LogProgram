31c56cea4d490f3fcc51c6024b0d2639c26b908a
yyyxon <130555434+yyyxon@users.noreply.github.com>
2023-07-26 11:14:34 +0900

    openLog, writeReport

---
+++ LogIO.java
@@ -0,0 +1,67 @@
+package report;
+
+import java.awt.FileDialog;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class LogIO {
+	
+	private String filePath;
+
+	//log파일인지 확인 후 파일 경로 저장
+	public void openLog() throws FileNotFoundException {
+		//filePath = "";
+		JFrame jm = new JFrame(); //임시 JFrame
+		FileDialog fdOpen = new FileDialog(jm/*JFrame*/, "Log File Open", FileDialog.LOAD);
+		fdOpen.setVisible(true);
+		
+		String fileName = fdOpen.getFile();
+		String fileDir = fdOpen.getDirectory();
+		
+		if(fdOpen!=null) {
+			//.log로 끝나는지 확인
+			if(fileName.substring(fileName.lastIndexOf(".")).equals(".log")) {
+				filePath = fileDir + fileName;
+			}else {
+				JOptionPane.showMessageDialog(null, "Log파일이 아닙니다.","Error Message",JOptionPane.ERROR_MESSAGE);
+			}			
+		}
+	 }
+	 
+	//레포트 생성
+	public void writeReport() throws IOException {
+		StringBuilder reportName = new StringBuilder("/report_").append(new Date().getTime()).append(".dat");
+		File file = new File("e:/dev/report");
+		
+		//디렉토리 경로가 존재하지 않으면 디렉토리 생성
+		if(!file.exists()) {
+			file.mkdirs();
+		}
+		
+		OutputStreamWriter osw = null;
+		
+		//지정한 경로와 이름으로 레포트 생성
+		try {
+			osw = new OutputStreamWriter(new FileOutputStream(file.getAbsolutePath() + reportName));
+			osw.write("레포트 내용~");
+			osw.flush();
+		}finally {
+			if(osw!=null) osw.close();
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			new LogIO().writeReport();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
